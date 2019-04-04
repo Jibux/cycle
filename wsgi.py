0d2d234a48649839b28f66bf1078d0f2212903e1
Jibux <jibux@jibux.info>
2019-04-04 14:43:59 +0200

    finish sql stuff

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,9 @@
 
 import os
 os.chdir(os.path.dirname(__file__))
-from bottle import app, run, post, request, response, get, route, template, HTTPResponse
+from bottle import app, hook, run, post, request, response, get, route, template, HTTPResponse
 import bottle
+from beaker.middleware import SessionMiddleware
 import json
 from pprint import pprint
 import configparser
@@ -14,28 +15,51 @@ import mysql.connector
 #import cgitb
 #cgitb.enable()
 
-application = bottle.app()
-
 data_dir = '/home/data/FertilityCare/'
 application_dir = '/home/jbh/Development/cycle'
 
-get_user_query = ("SELECT id FROM %s WHERE email='%s' AND password=PASSWORD(%s) AND enabled=1")
+session_opts = {
+    'session.type': 'file',
+    'session.cookie_expires': 300,
+    'session.data_dir': data_dir + '/sessions',
+    'session.auto': True
+}
 
-@application.route('/')
+#application = bottle.app()
+application = SessionMiddleware(bottle.app(), session_opts)
+
+
+@hook('before_request')
+def setup_request():
+    request.session = request.environ['beaker.session']
+
+@route('/')
 def hello():
     return "REST API for FertilityCare data"
 
-@application.route('/', method='OPTIONS')
+@route('/', method='OPTIONS')
 def trigger_method_options():
     return 0
 
-@application.route('/', method='POST')
+@route('/', method='POST')
 def process():
     pprint(request.json)
     action = request.json['action']
     print(action)
     credentials = request.json['credentials']
-    user_id = login_user(credentials.email, credentials.password)
+    if 'user_id' not in request.session:
+        print("New login")
+        login_status = login_user(credentials['email'], credentials['password'])
+        if login_status['status'] != 0:
+            print("Login failed: " + login_status['data'])
+            return bottle.HTTPResponse(status=500, body=login_status['data'])
+
+        request.session['user_id'] = login_status['data']
+    else:
+        print("Already logged in")
+
+    print("User id: " + str(request.session['user_id']))
+
     response = do_action(action)
     #data = {'2019216': {'comment': '',
     #                  'creationDate': 1552735477141,
@@ -48,7 +72,14 @@ def process():
     #                  'sticker': '0'}}
     return response
 
+def return_status(status, data=""):
+    return { 'status': status, 'data': data }
+
+def get_user_query(table_name='users'):
+    return ("SELECT id FROM " + table_name + " WHERE email=%s AND password=PASSWORD(%s) AND enabled=1")
+
 def login_user(email, password):
+    user_id = 0
     config = configparser.ConfigParser();
     config.read(application_dir + '/config.ini')
     db_config = {
@@ -58,22 +89,27 @@ def login_user(email, password):
         'database': config['DB']['db_name'],
         'port': config['DB']['db_port']
     }
-
+    
     try:
       cnx = mysql.connector.connect(**db_config)
     except mysql.connector.Error as err:
       if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
+        return return_status(1, "Something is wrong with your user name or password")
       elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
+        return return_status(1, "Database does not exist")
       else:
-        print(err)
+        return return_status(1, err)
     else:
-      cursor = cnx.cursor()
-      cursor.execute(get_user_query, (config['DB']['db_name'], email, password))
+      cursor = cnx.cursor(dictionary=True)
+      cursor.execute(get_user_query(), (email, password))
+      for (data) in cursor:
+          user_id = data['id']
       cursor.close()
       cnx.close()
-    return 1
+    if user_id == 0:
+        return return_status(1, "Invalid credentials")
+    else:
+        return return_status(0, user_id)
 
 def do_action(action):
     return {
@@ -90,5 +126,5 @@ def set_data():
 def unknown_action():
     return bottle.HTTPResponse(status=500, body='Unknown action')
 
-run(host='localhost', port=1234, debug=True)
+run(host='localhost', port=1234, debug=True, app=application)
 
