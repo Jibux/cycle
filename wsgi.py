da1e68cf964ae21dcc10087a4fadeaba22207923
Jibux <jibux@jibux.info>
2019-04-07 21:42:42 +0200

    return status

--- wsgi.py
+++ wsgi.py
@@ -58,8 +58,14 @@ def process():
     #                  'sticker': '0'}}
     return response
 
-def return_status(status, data=""):
-    return { 'status': status, 'data': data }
+def return_bottle(status, data=""):
+    bottle.HTTPResponse(status=status, body=data)
+
+def return_status(status=0, data=""):
+    data_to_return = { 'status': status, 'data': data }
+    print("Return status:")
+    pprint(data_to_return)
+    return data_to_return
 
 def get_user_query(table_name='users'):
     return ("SELECT id FROM " + table_name + " WHERE email=%s AND password=PASSWORD(%s) AND enabled=1")
@@ -77,21 +83,21 @@ def check_user_in_database(email, password):
     }
     
     try:
-      cnx = mysql.connector.connect(**db_config)
+        cnx = mysql.connector.connect(**db_config)
     except mysql.connector.Error as err:
-      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        return return_status(1, "Something is wrong with your user name or password")
-      elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        return return_status(1, "Database does not exist")
-      else:
-        return return_status(1, err)
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            return return_status(1, "Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            return return_status(1, "Database does not exist")
+        else:
+            return return_status(1, err)
     else:
-      cursor = cnx.cursor(dictionary=True)
-      cursor.execute(get_user_query(), (email, password))
-      for (data) in cursor:
-          user_id = data['id']
-      cursor.close()
-      cnx.close()
+        cursor = cnx.cursor(dictionary=True)
+        cursor.execute(get_user_query(), (email, password))
+        for (data) in cursor:
+            user_id = data['id']
+        cursor.close()
+        cnx.close()
     if user_id == 0:
         return return_status(1, "Invalid credentials")
     else:
@@ -100,31 +106,32 @@ def check_user_in_database(email, password):
 def do_action(data):
     action = data['action']
     print("ACTION: " + action)
-    function = {
-       'login' : login,
-       'GetData': get_data,
-       'SetData': set_data
-    }.get(data['action'], unknown_action)
-    return function(data)
+    if action == 'login':
+        return login(data['credentials'])
+    elif action == 'GetData':
+        return get_data()
+    elif action == 'SetData':
+        return set_data(data['data'])
+    else:
+        return unknown_action()
 
 def check_user_logged_in():
     return 'user_id' in request.session
 
-def login(data):
-    credentials = data['credentials']
+def login(credentials):
     if not check_user_logged_in():
         print("New login")
         login_status = check_user_in_database(credentials['email'], credentials['password'])
         if login_status['status'] != 0:
             print("Login failed: " + login_status['data'])
-            return bottle.HTTPResponse(status=500, body=login_status['data'])
+            return return_bottle(500, login_status['data'])
 
         request.session['user_id'] = login_status['data']
     else:
         print("Already logged in")
 
     print("User id: " + str(request.session['user_id']))
-    return {}
+    return return_status()
 
 def get_user_data_dir():
     return users_data_dir + '/' + str(request.session['user_id'])
@@ -132,23 +139,23 @@ def get_user_data_dir():
 def get_user_data_file():
     return get_user_data_dir() + '/' + user_data_file_name
 
-def get_data(data):
+def get_data():
     if not check_user_logged_in():
-        return bottle.HTTPResponse(status=403, body='User not logged in')
+        return return_bottle(403, 'User not logged in')
     else:
         user_data_dir = get_user_data_dir()
         user_data_file = get_user_data_file()
         if not os.path.isdir(user_data_dir):
-            return {}
+            return return_status(0, {})
         if not os.path.exists(user_data_file):
-            return {}
+            return return_status(0, {})
         with open(user_data_file) as json_file:
-            return json.load(json_file)
-        return {}
+            return return_status(0, json.load(json_file))
+        return return_status(0, {})
 
 def set_data(data):
     if not check_user_logged_in():
-        return bottle.HTTPResponse(status=403, body='User not logged in')
+        return return_bottle(403, 'User not logged in')
     else:
         user_data_dir = get_user_data_dir()
         user_data_file = get_user_data_file()
@@ -156,10 +163,10 @@ def set_data(data):
             pathlib.Path(user_data_dir).mkdir(parents=True, exist_ok=True)
         with open(user_data_file, 'w+') as json_file:
             json.dump(data, json_file)
-        return {}
+        return return_status()
 
 def unknown_action():
-    return bottle.HTTPResponse(status=500, body='Unknown action')
+    return return_bottle(500, 'Unknown action')
 
 run(host='localhost', port=1234, debug=True, app=application)
 
